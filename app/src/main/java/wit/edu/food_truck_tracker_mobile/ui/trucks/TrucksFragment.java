64aa6e8b54283e2e4fd94149f2868e3613f6af14
Jason Saturno <saturnoj@wit.edu>
2020-03-29 15:40:18 -0400

    Basic Truck list viewer

--- app/src/main/java/wit/edu/food_truck_tracker_mobile/ui/trucks/TrucksFragment.java
+++ app/src/main/java/wit/edu/food_truck_tracker_mobile/ui/trucks/TrucksFragment.java
@@ -1,40 +1,117 @@
 package wit.edu.food_truck_tracker_mobile.ui.trucks;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.config.LocationParams;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import wit.edu.food_truck_tracker_mobile.R;
+import wit.edu.food_truck_tracker_mobile.api.ApiClient;
+import wit.edu.food_truck_tracker_mobile.api.TrackerApi;
+import wit.edu.food_truck_tracker_mobile.models.Truck;
+import wit.edu.food_truck_tracker_mobile.ui.trucks.TrucksViewModel;
+import wit.edu.food_truck_tracker_mobile.ui.trucks.TruckCardAdapter;
 
 public class TrucksFragment extends Fragment {
 
     private static final int REQUEST_LOCATION = 99;
-    private TrucksViewModel homeViewModel;
+    private static final String RADIUS = "5";
+    private String latitude;
+    private String longitude;
+    private boolean called = false;
+    private TrucksViewModel galleryViewModel;
+    private List<Truck> trucks_list = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter truckAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
+        galleryViewModel =
                 new ViewModelProvider(this).get(TrucksViewModel.class);
         View root = inflater.inflate(R.layout.fragment_trucks, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            getLocationUpdates();
+        }
+        return root;
+    }
+
+    private void getLocationUpdates() {
+        SmartLocation.with(getContext()).location().config(LocationParams.NAVIGATION).start(new OnLocationUpdatedListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onLocationUpdated(Location location) {
+                if (location != null) {
+                    Log.d("TAG",location.toString());
+                    latitude = location.getLatitude() + "";
+                    longitude = location.getLongitude() + "";
+                    if (!called) {
+                        called = true;
+                        getTrucks();
+                    }
+                } else
+                    Toast.makeText(getActivity(), "Location is null", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        return root;
+    private void getTrucks() {
+        TrackerApi trackerApiService = ApiClient.getClient().create(TrackerApi.class);
+        Call<List<Truck>> getTrucks = trackerApiService.searchTrucks(latitude, longitude, RADIUS);
+        getTrucks.enqueue(new Callback<List<Truck>>() {
+            @Override
+            public void onResponse(Call<List<Truck>> call, Response<List<Truck>> response) {
+                List<Truck> trucks = response.body();
+                for(Truck truck: trucks){
+                    Log.d("TAG",truck.toString());
+                }
+                trucks_list.addAll(trucks);
+
+                recyclerView = getActivity().findViewById(R.id.trucks_list);
+                // use a linear layout manager
+                layoutManager = new LinearLayoutManager(getContext());
+                recyclerView.setLayoutManager(layoutManager);
+
+                // specify an adapter (see also next example)
+                truckAdapter = new TruckCardAdapter(trucks_list);
+                recyclerView.setAdapter(truckAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Truck>> call, Throwable t) {
+                Log.d("TAG", t.getMessage());
+            }
+        });
     }
 }
