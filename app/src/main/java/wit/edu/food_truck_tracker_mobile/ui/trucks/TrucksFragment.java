923daeb78bb64f9d90044863a8ee57ea5750c5f0
Jason Saturno <saturnoj@wit.edu>
2020-04-08 18:34:47 -0400

    Functioning Seekbar added

--- app/src/main/java/wit/edu/food_truck_tracker_mobile/ui/trucks/TrucksFragment.java
+++ app/src/main/java/wit/edu/food_truck_tracker_mobile/ui/trucks/TrucksFragment.java
@@ -8,11 +8,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,7 +38,6 @@ import wit.edu.food_truck_tracker_mobile.shared.TruckCardAdapter;
 public class TrucksFragment extends Fragment {
 
     private static final int REQUEST_LOCATION = 99;
-    private static final String RADIUS = "5";
     private String latitude;
     private String longitude;
     private boolean called = false;
@@ -44,21 +46,55 @@ public class TrucksFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter truckAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SeekBar seek_Bar;
+    private TextView radius_display;
+    private int nradius = 5;
+    private List<Truck> trucks;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         trucksViewModel =
                 new ViewModelProvider(this).get(TrucksViewModel.class);
         View root = inflater.inflate(R.layout.fragment_trucks, container, false);
+        seek_Bar = root.findViewById(R.id.seekbar);
+        radius_display = root.findViewById(R.id.radius);
+        seek_Bar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
 
+                    @Override
+                    public void onProgressChanged(android.widget.SeekBar seekBar, int progress, boolean fromUser) {
+
+                        nradius=progress;
+                        radius_display.setText(Integer.toString(nradius)+" m");
+                        trucks_list.removeAll(trucks);
+                        called = false;
+
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(android.widget.SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(android.widget.SeekBar seekBar) {
+
+                    }
+                }
+        );
 
         if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         } else {
             getLocationUpdates();
         }
+
+
         return root;
     }
+
+
 
     private void getLocationUpdates() {
         SmartLocation.with(getContext()).location().config(LocationParams.NAVIGATION).start(new OnLocationUpdatedListener() {
@@ -79,15 +115,20 @@ public class TrucksFragment extends Fragment {
     }
 
     private void getTrucks() {
+
         TrackerApi trackerApiService = ApiClient.getClient().create(TrackerApi.class);
-        Call<List<Truck>> getTrucks = trackerApiService.searchTrucks(latitude, longitude, RADIUS);
+
+        Call<List<Truck>> getTrucks = trackerApiService.searchTrucks(latitude, longitude, Integer.toString(nradius));
         getTrucks.enqueue(new Callback<List<Truck>>() {
+
             @Override
             public void onResponse(Call<List<Truck>> call, Response<List<Truck>> response) {
-                List<Truck> trucks = response.body();
+                trucks = response.body();
+
                 for(Truck truck: trucks){
                     Log.d("TAG",truck.toString());
                 }
+
                 trucks_list.addAll(trucks);
 
                 recyclerView = getActivity().findViewById(R.id.trucks_list);
@@ -104,6 +145,12 @@ public class TrucksFragment extends Fragment {
             public void onFailure(Call<List<Truck>> call, Throwable t) {
                 Log.d("TAG", t.getMessage());
             }
+
+
         });
+
     }
 }
+
+
+
